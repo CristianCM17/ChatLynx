b3ef89082b980e3230bd55fffad868c4a3010012
Francisco González Rodríguez <paco_glez02@hotmail.com>
2024-04-18 16:58:46 -0600

    Fix welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -12,87 +12,95 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        image: const DecorationImage(
-          image: AssetImage("assets/fondo_ws.png"),
-          fit: BoxFit.cover,
+        body: SingleChildScrollView(
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage("assets/fondo_ws.png"),
+            fit: BoxFit.cover,
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.green.shade900, Colors.black.withOpacity(0.9)],
+          ),
         ),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.green.shade900, Colors.black.withOpacity(0.9)],
-        ),
-      ),
-      child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 60),
-            width: MediaQuery.of(context).size.width * .8,
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Conecta Linces Fácil & Rápido',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 68,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 1,
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.07),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width * .8,
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Conecta Linces Fácil & Rápido',
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: size.width * 0.165,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 3.0,
+                            color: Colors.black.withOpacity(0.3),
+                            offset: const Offset(3.0, 3.0),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-            child: Text(
-              'Nuestro chat app es el camino perfecto para estar conectados como comunidad estudiantil.',
-              style: GoogleFonts.poppins(
-                color: Colors.white.withOpacity(0.5),
-                fontSize: 16,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 80,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/divider.png",
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              Text(
-                'INICIA SESIÓN',
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  height: 0.07,
-                  letterSpacing: 0.10,
+                  ],
                 ),
               ),
-              const SizedBox(
-                width: 30,
+            ),
+            SizedBox(height: size.height * 0.02),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                'Nuestro chat app es el camino perfecto para estar conectados como comunidad estudiantil.',
+                style: GoogleFonts.poppins(
+                  color: Colors.white.withOpacity(0.5),
+                  fontSize: size.width * 0.04,
+                  fontWeight: FontWeight.w300,
+                ),
               ),
-              Image.asset(
-                "assets/divider.png",
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const ButtonGoogle(),
-        ],
+            ),
+            SizedBox(height: size.height * 0.1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/divider.png",
+                ),
+                SizedBox(width: size.width * 0.07),
+                Text(
+                  'INICIA SESIÓN',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    height: 0.07,
+                    letterSpacing: 0.10,
+                  ),
+                ),
+                SizedBox(width: size.width * 0.07),
+                Image.asset(
+                  "assets/divider.png",
+                ),
+              ],
+            ),
+            SizedBox(height: size.height * 0.02),
+            Padding(
+              padding: EdgeInsets.only(bottom: size.height * 0.02),
+              child: ButtonGoogle(),
+            ),
+          ],
+        ),
       ),
     ));
   }
