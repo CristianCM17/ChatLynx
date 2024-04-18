064ac73c894e1c114bfd8703cc7097da0392eef5
Cristian Cardoso <cristiandavidcardoso@gmail.com>
2024-04-18 01:29:19 -0600

    conexion con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:chatlynx/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  //integracion con firebase
+  WidgetsFlutterBinding.ensureInitialized();  
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey:
+          "AIzaSyDi4PKYKwpSLtfrpV5nPgTd_p0fs_VDPu0", // paste your api key here
+      appId:
+          "com.example.chatlynx", //paste your app id here
+      messagingSenderId: "843525052315", //paste your messagingSenderId here
+      projectId: "chat-82a68", //paste your project id here
+    ),
+  );
+
+  runApp(MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
