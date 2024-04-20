f2535af76e8142246fbb4f59ec63e4776e52d192
Cristian Cardoso <cristiandavidcardoso@gmail.com>
2024-04-20 04:03:02 -0600

    verificar la duplicidad de uid

--- lib/widgets/button_google.dart
+++ lib/widgets/button_google.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:chatlynx/services/google_auth_firebase.dart'; // Asegúrate de importar UsersFirestore desde el archivo correcto
-import 'package:chatlynx/services/users_firestore.dart'; // Importa UsersFirestore
+import 'package:chatlynx/services/google_auth_firebase.dart'; 
+import 'package:chatlynx/services/users_firestore.dart'; 
 import 'package:google_fonts/google_fonts.dart';
 
 class ButtonGoogle extends StatefulWidget {
@@ -12,7 +12,7 @@ class ButtonGoogle extends StatefulWidget {
 
 class _ButtonGoogleState extends State<ButtonGoogle> {
   final GoogleAuthFirebase authGoogle = GoogleAuthFirebase();
-  final UsersFirestore usersFirestore = UsersFirestore(); // Crea una instancia de UsersFirestore
+  final UsersFirestore usersFirestore = UsersFirestore(); 
 
   @override
   Widget build(BuildContext context) {
@@ -20,29 +20,23 @@ class _ButtonGoogleState extends State<ButtonGoogle> {
       onPressed: () {
         authGoogle.loginWithGoogle(context).then((userInfo) {
           if (userInfo != null) {
-            // Aquí insertamos los datos del usuario en Firestore
-            usersFirestore.insertar({
-              'uid': userInfo['uid'],
-              'nombre': userInfo['nombre'], 
-              'email': userInfo['email'], 
-              'photoURL': userInfo['photoURL'],                
-            }).then((_) {
-              Navigator.pushNamed(context, "/home", arguments: userInfo);
-
-              var snackbar = SnackBar(
-                content: Text(
-                  "Iniciaste Sesión como: ${userInfo['nombre'] ?? 'Desconocido'}",
-                  style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                      fontSize: 14),
-                ),
-                duration: const Duration(seconds: 4),
-                backgroundColor: Colors.green.shade300,
-                behavior: SnackBarBehavior.floating,
-                margin: const EdgeInsets.only(bottom: 50, left: 20, right: 20),
-              );
-              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            usersFirestore.consultar().first.then((snapshot) {
+              bool uidExists = snapshot.docs.any((doc) => doc['uid'] == userInfo['uid']);
+              if (!uidExists) {
+                usersFirestore.insertar({
+                  'uid': userInfo['uid'],
+                  'nombre': userInfo['nombre'], 
+                  'email': userInfo['email'], 
+                  'photoURL': userInfo['photoURL'],                
+                }).then((_) {
+                  Navigator.pushNamed(context, "/home", arguments: userInfo);
+                  showLoginSnackBar(context, userInfo);
+                });
+              } else {
+                print('El UID ${userInfo['uid']} ya existe en la colección "users".');
+                 Navigator.pushNamed(context, "/home", arguments: userInfo);
+                 showLoginSnackBar(context, userInfo);
+              }
             });
           } else {
             print('Inicio de sesión con Google fallido');
@@ -73,4 +67,20 @@ class _ButtonGoogleState extends State<ButtonGoogle> {
       ),
     );
   }
+  void showLoginSnackBar(BuildContext context, Map<String, dynamic> userInfo) {
+  var snackbar = SnackBar(
+    content: Text(
+      "Iniciaste Sesión como: ${userInfo['nombre'] ?? 'Desconocido'}",
+      style: GoogleFonts.poppins(
+          fontWeight: FontWeight.w500,
+          color: Colors.black,
+          fontSize: 14),
+    ),
+    duration: const Duration(seconds: 4),
+    backgroundColor: Colors.green.shade300,
+    behavior: SnackBarBehavior.floating,
+    margin: const EdgeInsets.only(bottom: 50, left: 20, right: 20),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackbar);
+}
 }
