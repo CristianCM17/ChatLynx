a752bbac2263f4049261e3ed2f2798a9028d2abd
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-04-30 16:36:57 -0600

    Add videollamadas parciales

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,35 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
 class CallScreen extends StatefulWidget {
- // final Map<String,dynamic>? arguments;
+  // final Map<String,dynamic>? arguments;
 
-  const CallScreen({super.key, });
-
-  
+  const CallScreen({
+    super.key,
+  });
 
   @override
   State<CallScreen> createState() => _CallScreenState();
 }
 
-class _CallScreenState extends State<CallScreen> {
-  
+String userID = Random().nextInt(10000).toString();
 
-  
-  
+class _CallScreenState extends State<CallScreen> {
   @override
   Widget build(BuildContext context) {
     //String? currentUid = widget.arguments!["currentUid"];
-  // String? otherUid = widget.arguments!["otherUid"];
+    // String? otherUid = widget.arguments!["otherUid"];
 
-   //List<String?> ids= [currentUid, otherUid];
-  // ids.sort();
-   //String callId=ids.join("_");
+    //List<String?> ids= [currentUid, otherUid];
+    // ids.sort();
+    //String callId=ids.join("_");
     return ZegoUIKitPrebuiltCall(
-       appID: 1516252591,
-       appSign: "a493ad68d54dab428ef205bfb4ea2518c4424e109d54820c0d559d1d0e14cc55", 
-       callID: "123", 
-       userID: "Cristian123", 
-       userName: "Cristian", 
-       config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall());
+        appID: 1516252591,
+        appSign:
+            "a493ad68d54dab428ef205bfb4ea2518c4424e109d54820c0d559d1d0e14cc55",
+        callID: "123",
+        userID: "user_$userID",
+        userName: "Cristian",
+        config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall());
   }
 }
